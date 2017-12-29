46c2abebd57674e3079c35cef088669e7c3fbb17
jomesan <mellado.proyecto2017@gmail.com>
2017-12-29 22:44:42 +0100

    con entidades sin relacion

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,21 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { PruebaJhTablaTrenesModule } from './tabla-trenes/tabla-trenes.module';
+import { PruebaJhEstacionModule } from './estacion/estacion.module';
+import { PruebaJhLineaModule } from './linea/linea.module';
+import { PruebaJhEventoModule } from './evento/evento.module';
+import { PruebaJhFechaModule } from './fecha/fecha.module';
+import { PruebaJhTrenModule } from './tren/tren.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        PruebaJhTablaTrenesModule,
+        PruebaJhEstacionModule,
+        PruebaJhLineaModule,
+        PruebaJhEventoModule,
+        PruebaJhFechaModule,
+        PruebaJhTrenModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
